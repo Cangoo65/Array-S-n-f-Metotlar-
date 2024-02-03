a277f327aab95f706df42127143c257b8aebd166
Cangoo65 <caneraytekin06@hotmail.com>
2024-02-03 20:42:21 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int[] sayilar = { 12, 24, 1, 99, 15, 21, 56 };
+
+Console.WriteLine("****** Normal Gösterim *******");
+foreach (var item in sayilar)
+    Console.WriteLine(item);
+
+Console.WriteLine("****** Sıralı Gösterim *******");
+Array.Sort(sayilar);
+foreach (var item in sayilar)
+    Console.WriteLine(item);
+Console.WriteLine("****** Ters Sıralı Gösterim *******");
+Array.Reverse(sayilar);
+foreach (var item in sayilar)
+    Console.WriteLine(item);
+Console.WriteLine("****** Clear Gösterim *******");
+Array.Clear(sayilar,2,2);
+foreach (var item in sayilar)
+    Console.WriteLine(item);
+Console.WriteLine("****** Indexof Gösterim *******");
+
+    Console.WriteLine(Array.IndexOf(sayilar, 99));
+
+Console.WriteLine("****** Resize Gösterim *******");
+Array.Resize<int>(ref sayilar,3);
+foreach (var item in sayilar)
+    Console.WriteLine(item);
